c81cafef0e1d1818045e9d89557a5e9edc80aa26
Namrath <namrathracharla1@gmail.com>
2023-12-24 23:06:19 +0530

    sync

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Navbar/Header"
 import Login from "./components/Signup/Login"
-import Header from "./components/Header"
+
 import Footer from "./components/Footer"
 function App() {
 
@@ -10,28 +10,7 @@ function App() {
     <Header/>
     <Login />
     
-    <div className="w-[50%]">
-    In 2021, Jose, a former boxer from the UAE, discussed the power of CBD on an online platform. He spoke about how it could reduce inflammation and ease tight muscles, thus treating the cause of pain rather than just the pain itself.
- 
-
- Shikhar, an entrepreneur from India, joined the conversation. He shared that the Vijaya plant, an important source of CBD, has been praised for its medicinal value in ancient scriptures since 3rd millennium BCE. It is also recognised as an Ayurvedic medicine in India.
-  
- 
- The duo soon met Bhavana, a scientist from Canada. She corrected someone that Cannabis sativa Linn. (Vijaya) does not cure COVID-19, but is effective in managing symptoms like body ache, lack of appetite, and the general anxiety around the pandemic.
-  
- 
- Jose and Shikhar discovered that Bhavana had conducted extensive research on the medical benefits of the Vijaya plant for over a decade. She had spent her time formulating products that could leverage the plant's benefits to address various medical conditions.
-  
- 
- The three of them then decided to work together and create Gaanj wellness, a brand of wellness products & medicines that provides people with a safe, natural and effective alternative to allopathic & opioid based medicines.
- 
- Cannabis, Hemp, Vijaya, Bhaang…
- Cannabis, more commonly known as Bhaang or Vijaya, is a plant that originated from the Indian subcontinent. Its use can be traced back to as early as the 3rd millennium BCE. It has been used in our country for medicinal, commercial, recreational, and spiritual purposes. Vijaya in Sanskrit means victory. The plant got its name because it is believed to help conquer Arishadvarga or the six enemies of the mind, namely desire, anger, greed, arrogance/ego, delusion and jealousy. This is why the Atharva Veda referred to Vijaya as one of the five sacred plants necessary for humanity. 
- 
-  
- 
- Vijaya has a long history of traditional use in Ayurvedic medicine, a system of natural healing that has been practiced in India for thousands of years. In Ayurveda, cannabis is known as bhang and is considered a medicinal herb with a range of therapeutic properties. According to Ayurvedic principles, cannabis is believed to have a balancing effect on the body and mind, and can be used to treat a variety of conditions, including pain, inflammation, anxiety, and insomnia. 
-    </div>
+    
     <Footer/>
     </>
   )
